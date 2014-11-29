859cbe2c972cae384274c852628bf77663613f09
lurehvt <magdalena.zak@gmail.com>
2014-11-29 22:39:04 +0000

    bracket expression brackets can be closed with other specified brackets

--- src/Brackets/BracketsWord.java
+++ src/Brackets/BracketsWord.java
@@ -11,6 +11,15 @@ public class BracketsWord {
     };
     public static HashSet<Character> additionalLetters = new HashSet<Character>(){{add('a');add('b');}};
 
+    public static HashMap<Character, List<Character>> bracketsExtended = new HashMap<Character, List<Character>>(){
+        {
+            put('(', Arrays.asList(')',']', '[') );
+            put('{', Arrays.asList('>',']','(') );
+            put('[', Arrays.asList(')','}') );
+            put('<', Arrays.asList('>') );
+        }
+    };
+
     public boolean IsBracketWord( String word){
         HashMap<Character, Character> closingBrackets = new HashMap<Character, Character>();
         //assuming unique values in brackets map
@@ -30,5 +39,71 @@ public class BracketsWord {
             }
         }
         return notMatched.isEmpty();
+    }
+
+    public boolean IsBracketWordExtended( String word){
+        HashMap<Character, List<Character>> closingBrackets = new HashMap<Character, List<Character>>();
+        //assuming unique values in brackets map
+        for (Map.Entry<Character, List<Character>> entry : bracketsExtended.entrySet())
+            for (Character end : entry.getValue()) {
+                closingBrackets.putIfAbsent(end, Arrays.asList(entry.getKey()));
+                closingBrackets.get(end).add(entry.getKey());
+            }
+
+        Stack<Character> notMatched = new Stack<Character>();
+        char[] letters = word.toCharArray();
+
+        return IsBracketWordExtended(letters, notMatched, closingBrackets);
+    }
+
+    private boolean IsBracketWordExtended(char[] letters, Stack<Character> notMatched, HashMap<Character, List<Character>> closingBrackets) {
+        if( letters.length == 0 )
+            return notMatched.isEmpty();
+        //skip non brackets
+        int i = 0;
+        while(!IsBracketLetter( letters[i], closingBrackets) && i < letters.length) i++;
+        if( i == letters.length) return notMatched.isEmpty();
+
+
+        if( notMatched.isEmpty()){
+            if( additionalLetters.contains(letters[i]) || bracketsExtended.containsKey(letters[i])){
+                notMatched.push(letters[i]);
+                return IsBracketWordExtended(Arrays.copyOfRange(letters, i+1, letters.length-1), notMatched, closingBrackets);
+            }
+            else return false; //closing bracket with no opening bracket to match
+        }
+        else{
+            if( (additionalLetters.contains(letters[i]) && notMatched.peek() == letters[i])){
+                notMatched.pop();
+                return IsBracketWordExtended(Arrays.copyOfRange(letters, i+1, letters.length-1), notMatched, closingBrackets);
+            }
+            if( closingBrackets.containsKey(letters[i]) && closingBrackets.get(letters[i]).contains( notMatched.peek()) ) {
+                if(!bracketsExtended.containsKey(letters[i])){
+                    notMatched.pop();
+                    return IsBracketWordExtended(Arrays.copyOfRange(letters, i+1, letters.length-1), notMatched, closingBrackets);
+                }
+                else{
+                    //branch:
+                    notMatched.push(letters[i]);
+                    boolean openingBracketBranch =
+                            IsBracketWordExtended(Arrays.copyOfRange(letters, i+1, letters.length-1), notMatched, closingBrackets);
+                    if(openingBracketBranch) return true;
+                    //closing bracket branch:
+                    notMatched.pop();
+                    return IsBracketWordExtended(Arrays.copyOfRange(letters, i+1, letters.length-1), notMatched, closingBrackets);
+
+                }
+            }
+            else {
+                //opening bracket, and not closing
+                notMatched.push(letters[i]);
+                return IsBracketWordExtended(Arrays.copyOfRange(letters, i+1, letters.length-1), notMatched, closingBrackets);
+            }
+
+        }
+    }
+
+    private boolean IsBracketLetter(char c, HashMap<Character, List<Character>> closingBrackets) {
+        return closingBrackets.containsKey(c) || bracketsExtended.containsKey(c) || additionalLetters.contains(c);
     }
 }
--- src/Brackets/BracketsWordTest.java
+++ src/Brackets/BracketsWordTest.java
@@ -2,6 +2,9 @@ package Brackets;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class BracketsWordTest {
@@ -26,6 +29,37 @@ public class BracketsWordTest {
             for (String testCase : testCasesWrong) {
                 assertEquals("For string " + testCase + " expected output " + false, false, bracketsWord.IsBracketWord(testCase));
             }
+            List<Character> a  = Arrays.asList(')', ']');
+
+    }
+
+    public void testIsBracketWordExtended() throws Exception {
+//        put('(', Arrays.asList(')',']', '[') );
+//        put('{', Arrays.asList('>',']','(') );
+//        put('[', Arrays.asList(')','}') );
+//        put('<', Arrays.asList('>') );
+        BracketsWord bracketsWord = new BracketsWord();
+        String[] testCases = new String[]{
+                "sklgjd", "", "{egftw}", "{fe[vd]}",  "{{[([])]}}",
+                "{fe[]}",  "{fe[()]}",  "sakjla", "(aa)abba", "(dsfb{bbaa}[]fgb)rg",
+                "(dkfj]", "(dkfj[sdf]]", "(aabaab]",
+                "<fg{r(g[[oi)}fd>"
+        };
+
+        for (String testCase : testCases) {
+            assertEquals("For string " + testCase + " expected output " + true, true, bracketsWord.IsBracketWordExtended(testCase));
+        }
+
+        String[] testCasesWrong = new String[]{
+                "sklg)(jd", "[{", "egftw}", "{fevd]}",  "{{[([])]}}}",
+                "{fe]}",  "{fe[()}",  "sakjlaa", "(aa)aba", "(dsfb{bbaa}[]fgba)rg",
+                "(dkfj>", "(dkfj[sdf>]", "(aabab]",
+                "<fg{r(g[]oi)}fd>"
+        };
+
+        for (String testCase : testCasesWrong) {
+            assertEquals("For string " + testCase + " expected output " + false, false, bracketsWord.IsBracketWordExtended(testCase));
+        }
 
     }
 }
