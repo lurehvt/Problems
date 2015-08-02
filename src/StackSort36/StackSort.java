09896648eba4394cfab0b469507100e2ba2c1de8
lurehvt <magdalena.zak@gmail.com>
2015-08-02 16:04:16 +0100

    problem 3.6

--- src/StackSort36/StackSort.java
+++ src/StackSort36/StackSort.java
@@ -3,21 +3,21 @@ package StackSort36;
 import java.util.Stack;
 
 /**
- * Created by Magda on 2015-07-23.
+ * Created by M on 2015-07-23.
  */
-public class StackSort {
-    void SortAscending( Stack<Integer> stack){
-        Stack<Integer> tmpStack = new Stack<Integer>();
+public class StackSort<T extends Comparable<T>> {
+    void SortAscending( Stack<T> stack){
+        Stack<T> tmpStack = new Stack<T>();
 
         if( stack.isEmpty())
             return;
 
-        Integer curr;
+        T curr;
         boolean sorted = false;
 
         while( !sorted){
             curr = stack.peek();
-            while (!stack.isEmpty() && stack.peek() <= curr) {
+            while (!stack.isEmpty() && stack.peek().compareTo(curr) <= 0) {
                 curr = stack.peek();
                 tmpStack.push(stack.pop());
             }
@@ -25,12 +25,10 @@ public class StackSort {
                 sorted = true;
             }
             else{
-                Integer tmp = stack.pop();
-                while (!tmpStack.isEmpty() && tmp > tmpStack.peek())
+                T tmp = stack.pop();
+                while (!tmpStack.isEmpty() && tmp.compareTo(tmpStack.peek()) > 0 )
                     stack.push(tmpStack.pop());
                 stack.push(tmp);
-                while (!tmpStack.isEmpty())
-                    stack.push(tmpStack.pop());
             }
         }
         while (!tmpStack.isEmpty())
--- src/StackSort36/StackSortTest.java
+++ src/StackSort36/StackSortTest.java
@@ -9,14 +9,26 @@ import java.util.Stack;
 
 import static org.junit.Assert.*;
 
+class StackCreator {
+    public
+    static <T> Stack<T> create(T... values) {
+        Stack<T> result = new Stack<T>();
+        for (T value : values) {
+          result.push(value);
+        }
+        return result;
+    }
+}
+
 public class StackSortTest {
     @org.junit.Test
+
     public void testRepeats() throws Exception {
         StackSort stackSort = new StackSort();
-
+            Stack<Integer> s = StackCreator.create(1, 2, 3);
 
             Stack<Integer>[][] testCases = new Stack[][]{
-                    new Stack[]{new Stack(){{push(4);push(5);push(1);}}, new Stack(){{push(1);push(4);push(5);}} },
+                    new Stack[]{StackCreator.create(4, 5, 1), new Stack(){{push(1);push(4);push(5);}} },
                     new Stack[]{new Stack(){{push(1);push(1);push(1);}}, new Stack(){{push(1);push(1);push(1);}} },
                     new Stack[]{new Stack(){{push(4);push(3);push(2);push(1);}}, new Stack(){{push(1);push(2);push(3);push(4);}} },
                     new Stack[]{new Stack(){}, new Stack(){ } },
@@ -28,6 +40,12 @@ public class StackSortTest {
         for (Stack<Integer>[] testCase : testCases) {
             stackSort.SortAscending(testCase[0]);
             assertEquals(testCase[0], testCase[1]);
+        }
+    }
+
+    private void pushElements( Integer... elems){
+        for (Integer elem : elems){
+
         }
     }
 }
